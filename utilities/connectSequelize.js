420478701e35bbbfc4b59760e6adfbf90f443600
agent-indigo <bradenhynes@gmail.com>
2024-10-20 01:12:21 -0230

    Cleanup

--- utilities/connectSequelize.js
+++ utilities/connectSequelize.js
@@ -1,17 +1,15 @@
 import sequelize from './sequelize.js'
 const connectSequelize = async () => {
   let connected = false
-  if (!connected) {
-    try {
-      await sequelize.authenticate()
-      connected = true
-      console.log('Sequelize successfully connected.')
-      await sequelize.sync()
-      console.log('Schema successfully synchronized.')
-    } catch (error) {
-      console.error(error)
-      process.exit(1)
-    }
+  if (!connected) try {
+    await sequelize.authenticate()
+    connected = true
+    console.log('Sequelize successfully connected.')
+    await sequelize.sync()
+    console.log('Schema successfully synchronized.')
+  } catch (error) {
+    console.error(error.toString())
+    process.exit(1)
   }
 }
 export default connectSequelize
