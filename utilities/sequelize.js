d63b547c1fe6106b7ed5c949de41726e99fabf35
agent-indigo <bradenhynes@gmail.com>
2024-11-21 21:39:03 -0330

    Cleanup

--- utilities/sequelize.js
+++ utilities/sequelize.js
@@ -1,11 +1,11 @@
-import Sequelize from 'sequelize'
+import {Sequelize} from 'sequelize'
 const sequelize = new Sequelize({
   database: process.env.DB_NAME ?? 'invoices',
   username: process.env.DB_USER ?? 'postgres',
   password: process.env.DB_PW ?? '',
   host: process.env.DB_HOST ?? 'localhost',
-  port: process.env.DB_PORT ?? '5432',
-  dialect: process.env.DB_DIALECT ?? 'postgres',
+  port: parseInt(process.env.DB_PORT ?? '5432'),
+  dialect: 'postgres',
   dialectOptions: {
     ssl: {
       prefer: true
