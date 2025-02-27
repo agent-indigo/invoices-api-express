e2971cf1d511a227ae099be206242643c7f8ac6a
agent-indigo <bradenhynes@gmail.com>
2025-02-27 19:29:57 -0330

    Bug fix

--- middleware/sendErrorResponses.js
+++ middleware/sendErrorResponses.js
@@ -7,6 +7,6 @@ const sendErrorResponses = (
 .status(response.statusCode === 200 ? 500 : response.statusCode)
 .json({
   message: error.message,
-  stack: process.env.NODE_ENV === 'production' ? null : error.stack
+  stack: process.env.NODE_ENV === 'production' ? undefined : error.stack
 })
 export default sendErrorResponses
