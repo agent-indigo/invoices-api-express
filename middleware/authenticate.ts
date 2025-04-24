2fcfa7618ca53934ac447a21165181dc7fb7b585
agent-indigo <bradenhynes@gmail.com>
2025-04-24 19:02:32 -0230

    Improved JWT token generation and validation

--- middleware/authenticate.ts
+++ middleware/authenticate.ts
@@ -20,13 +20,42 @@ const authenticate: RequestHandler = catchRequestErrors(async (
       token,
       process.env.JWT_SECRET ?? 'd3v3l0pm3nt53cr3tk3yn0t53cur3@t@11n3v3ru53!npr0duct!0n3v3r!!!'
     )
-    const user: Model<UserSqlRecord> | null = await userSqlModel.findByPk(typeof decoded === 'string' ? decoded : '')
-    if (user) {
-      request.params.userId = user.getDataValue('id') ?? ''
-      next()
+    if (typeof decoded !== 'string') {
+      const {
+        iss,
+        aud,
+        sub,
+        nbf,
+        exp
+      }: JwtPayload = decoded
+      if (iss !== 'invoices.api') {
+        response.status(401)
+        throw new Error('Invalid token issuer.')
+      } else if (aud !== 'invoices.client') {
+        response.status(401)
+        throw new Error('Invalid token audience.')
+      } else if (typeof nbf !== 'number' || typeof exp !== 'number') {
+        response.status(401)
+        throw new Error('Token unreadable.')
+      } else if (nbf > Math.floor(Date.now() / 1000)) {
+        response.status(401)
+        throw new Error('Token not yet valid.')
+      } else if (exp < Math.floor(Date.now() / 1000)) {
+        response.status(401)
+        throw new Error('Token expired.')
+      } else {
+        const user: Model<UserSqlRecord> | null = await userSqlModel.findByPk(sub)
+        if (user) {
+          request.params.userId = user.getDataValue('id') ?? ''
+          next()
+        } else {
+          response.status(401)
+          throw new Error('User not found.')
+        }
+      }
     } else {
       response.status(401)
-      throw new Error('User not found.')
+      throw new Error('Token unreadable.')
     }
   } else {
     response.status(401)
--- utilities/createToken.ts
+++ utilities/createToken.ts
@@ -1,11 +1,19 @@
+import {randomUUID} from 'crypto'
 import {Response} from 'express'
 import jwt from 'jsonwebtoken'
 const createToken: Function = (
   response: Response,
   id: string
 ): string => {
-  const token = jwt.sign(
-    id,
+  const token = jwt.sign({
+    jti: randomUUID().toString(),
+    iss: 'invoices.api',
+    aud: 'invoices.client',
+    sub: id,
+    iat: Math.floor(Date.now() / 1000),
+    nbf: Math.floor(Date.now() / 1000),
+    exp: Math.floor(Date.now() / 1000) + 2592000
+  },
     process.env.JWT_SECRET ?? 'd3v3l0pm3nt53cr3tk3yn0t53cur3@t@11n3v3ru53!npr0duct!0n3v3r!!!'
   )
   response.cookie(
@@ -14,7 +22,7 @@ const createToken: Function = (
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production' ? true : false,
       sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000
+      maxAge: 25920000,
     }
   )
   return token
